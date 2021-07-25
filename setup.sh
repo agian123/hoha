fcc81c1028cefb0de2afde2175b07f8c8e0b6aaf
agian123 <85033259+agian123@users.noreply.github.com>
2021-07-25 07:57:50 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,24 +10,14 @@ fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
-IZIN=$( curl http://lnd.red-flat.my.id:81/shahwjusisjsnajakajannaiajanajam | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Please Contact Admin"
-echo "Telegram t.me/bokirtampan"
-rm -f setup.sh
-exit 0
-fi
+
 if [ -f "/etc/v2ray/domain" ]; then
 echo "Script Already Installed"
 exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/agian123/hoha/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
